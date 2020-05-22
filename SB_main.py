bb25082b2a6b2cf75ceb0da337c9dcf318cb03d8
cicero <ciceroufpe@gmail.com>
2020-05-22 14:26:33 -0300

    Print changes

--- SB_main.py
+++ SB_main.py
@@ -2,5 +2,9 @@ from impress import FineScaleMesh as mesh
 from packs.stokes_brinkman_3d import stokes_brinkman as stokes
 
 file='./mesh/9x9x9.msh'
+print("Preprocessing mesh")
 M = mesh(file)
+print("Preprocessing Mesh Finished")
+print("Applying Stokes-Brinkmann")
 stokes.stokes_solver(M)
+print("Applying stokes_brinkman finished")
