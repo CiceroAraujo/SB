1824b6c7c1250704a1358006f61691b0f5d1eb3e
cicero <ciceroufpe@gmail.com>
2020-05-22 16:46:41 -0300

    nothing changed

--- SB_main.py
+++ SB_main.py
@@ -5,6 +5,6 @@ file='./mesh/9x9x9.msh'
 print("Preprocessing mesh")
 M = mesh(file)
 print("Preprocessing Mesh Finished")
-print("Applying Stokes-Brinkmann")
+print("Applying Stokes-Brinkmann")#
 stokes.stokes_solver(M)
 print("Applying stokes_brinkman finished")
